8fa3f3e939767c853804840506938bcf25a5c9c5
Gaspare <gaspare92@me.com>
2020-04-10 21:23:38 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Gaspare Monte on 10/04/2020.
+//  Copyright © 2020 Gaspare Monte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
